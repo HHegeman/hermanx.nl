e678f06240ae6ded9c47c677360a46062aa11cf8
HHegeman <hhegeman@gmail.com>
2014-10-27 14:10:14 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hermanx::Application.config.secret_key_base = 'bfb674cef994101df26880181cbc1dc11f25acf8dc436f81133f64d7a5d18e671bb6588392cf41bac779027d4937b7ffd72cb6c48cd048b9a5c7419e756eedbe'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Hermanx::Application.config.session_store :cookie_store, key: '_hermanx_session'
